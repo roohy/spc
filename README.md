fe596ec37b6a15ce5052eee687a59de9902a2417
Ruhollah Shemirani <shemirani.r@gmail.com>
2025-05-31 19:44:02 -0400

    alpha v1

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ This repository includes scripts to calculate SPCs using IBD data.
 
 ## Dependencies
 
-To run the main script `calculate_spc.py`, you need the following packages on Python (version 3.5 and above required):
+To run the main script `calculate_spc.py`, the following Python packages are needed (Python 3.5 and above required):
 
 - [Numpy](https://numpy.org/install/) (tested on 1.11 and later)
 - [Networkit](https://networkit.github.io) (tested on 8.0 and later)
 - [Scipy](https://scipy.org) (tested on 1.5.4 and later)
 
-All packages can be installed using Python's package manager `pip`. We suggest setting up a new environment. This process is made convenient through Conda:
+All packages can be installed using Python's package manager `pip`. Setting up a new environment is preferred. If Conda is available, new environments can be set using the following commands:
 
 ```
 conda create -n spc_env python=3.9
@@ -31,7 +31,23 @@ pip install numpy==1.22.3 scipy==1.8.1 networkit==10.0
 
 ## Generating Global IBD network
 
-The script called `generate_global_network.py` accepts a path to a directory that includes IBD segment information for all chromosomes divides in files. The IBD segment files should end in `.match` format. We currently only support iLASH output. The script can be modified to support other software. You can also contact us.The second argument for the script is the address for the resulting global network.
+The script called `generate_global_network.py` can generate the similarity network file required for SPC calculation. It can be run using the following command:
+
+```
+$ python generate_global_network.py
+```
+The script has the following arguments:
+
+- `--ibd-file-address`/`-i`: the address of the IBD file(s). If there are multiple IBD files (e.g. separated by chromosome),replace the chromosome number, or any other numerical index, with '@' in the address. E.g. /path/to/files/ibd_output_chr@.match
+- `id1-column`,`id2-column`, `--ibd-column`: column index for the sample IDs of the individuals, along with that of the IBD length column, within the IBD file.
+- `haploid-mode`: Use this flag if haplotypes codes have their own column, instead of a haplotype prefix within the same columns.
+- `minimum-segment-length`: minimum length of IBD segments to be counted in the aggregation (default:3).
+- `-minimum-agg-length`: minimum total length of IBD shared between a pair of individuals to be considered as an entry in the output file (defualt:6).
+- `--sep`: separator in the IBD file. Use 't' for tab and 's' for space (default:'s').
+- `--removal-file`:address of a file the includes sample IDs of the participants that should be excluded from the final file. e.g. closely related individuals
+- `--output`: address for the output file.
+
+<!-- accepts a path to a directory that includes IBD segment information for all chromosomes in separate files. The IBD segment files should end in `.match` format. We currently only support iLASH output. The script can be modified to support other software. You can also contact us.The second argument for the script is the address for the resulting global network. -->
 
 ## Calculating SPCs
 
--- generate_global_network.py
+++ generate_global_network.py
@@ -1,25 +1,41 @@
-import sys,os
+import os,argparse,gzip,glob
 
-def write_file(links,output_addr):
+def read_relateds(related_file_addr):
+    relateds = set()
+    with open(related_file_addr,'r') as related_file:
+        for line in related_file:
+            relateds.add(line.strip())
+    return relateds
+
+def write_file(links,output_addr,min_length=6.0):
     count = 0
     with open(output_addr,'w') as output_file:
         for id1 in links:
             for id2 in links[id1]:
                 count += 1
-                if links[id1][id2] >= 6.0:
+                if links[id1][id2] >= min_length:
                     output_file.write(f'{id1}\t{id2}\t{links[id1][id2]}\n')
     print(f'Total connection count is {count}')
 
-def make_links(input_addr):
+def make_links(ibd_addr_list,id1_col=1,id2_col=3,ibd_col=-2,haploid_mode=False,min_len=3.0,sep=' ',removals=set()):
     links = {}
-    match_files_addr_list = [os.path.join(input_addr,f) for f in os.listdir(input_addr) if os.path.isfile(os.path.join(input_addr, f)) and f.endswith('.match')]
-    for addr in match_files_addr_list:
-        with open(addr,'r') as match_file:
+    # match_files_addr_list = [os.path.join(input_pre,f) for f in os.listdir(input_pre) if os.path.isfile(os.path.join(input_pre, f)) and f.endswith(f'{input_suff}')]
+    # for addr in match_files_addr_list:
+    for addr in ibd_addr_list:
+        with gzip.open(addr,'rt') as match_file:
             for line in match_file:
-                data = line.strip().split()
-                id1 = data[1][:-2]
-                id2 = data[3][:-2]
-                link_length = float(data[-2])
+                data = line.strip().split(sep)
+                link_length = float(data[ibd_col])
+                if link_length < min_len:
+                    continue
+                if haploid_mode:
+                    id1= data[id1_col]
+                    id2 = data[id2_col]
+                else:
+                    id1 = data[id1_col][:-2]
+                    id2 = data[id2_col][:-2]
+                if id1 in removals or id2 in removals:
+                    continue
                 if id1 in links:
                     if id2 in links[id1]:
                         links[id1][id2] += link_length
@@ -38,9 +54,46 @@ def make_links(input_addr):
                     links[id1][id2] = link_length
     return links
 def main():
-    links = make_links(sys.argv[1])
-    write_file(links,sys.argv[2])
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--output','-o',help='Address for the output network file',type=str,required=True)
+    parser.add_argument('--ibd-file-address','-i',help='Address for IBD files. If there are separated by chromosome, replace the chromosome number with @ in the address',type=str,required=True,dest='ibd_addr')
+    # parser.add_argument('--ibd-file-suffix','-s',help='Suffix for the IBD file addresses (optional)',type=str,default='.match',required=False,dest='ibd_suff')
+    parser.add_argument('--id1-column',help='Zero-based column index for the first ID column',type=int,default=1,dest='id1_col')
+    parser.add_argument('--id2-column',help='Zero-based column index for the second ID column',type=int,default=3,dest='id2_col')
+    parser.add_argument('--ibd-column',help='Zero-based column index for the aggregate IBD informaiton',type=int,default=-2,dest='ibd_col')
+    parser.add_argument('--haploid-mode',help='Set this flag if the haplotype codes have the own column and are not appended to the individuals IDs',action='store_true',dest='haploid_mode')
+    parser.add_argument('--minimum-segment-length',help='minimum length of IBD segments to be considered for aggregation',type=float,default=3,dest='min_seg_len')
+    parser.add_argument('--minimum-agg-length',help='minimum aggreagate length o IBD sharing to be considered as an edge',type=float,default=6.0,dest='min_agg_len')
+    parser.add_argument('--sep',help='Separator. Leave t for tab and s for space',type=str,default='s')
+    parser.add_argument('--removal-file',help='address for the file containing individuals to remove',type=str,default='',dest='removal_file_addr')
+    # parser.add_argument('--cluster','-c',help='Address of the file that includes member IDs',type=str,required=True)
+    
+    # parser.add_argument('')
+
+    args = parser.parse_args()
+    sep = args.sep
+    if sep == 's':
+        sep=' '
+    if sep =='t':
+        sep = '\t'
+    if args.removal_file_addr == '':
+        removals = set()
+    else:
+        removals = read_relateds(args.removal_file_addr)
+    ibd_addr_list = None
+    if '@' in args.ibd_addr:
+        addr_pattern = args.ibd_addr.replace('@','*')
+        ibd_addr_list = glob.glob(addr_pattern)
+    else:
+        ibd_addr_list = [args.ibd_addr]
+    links = make_links(ibd_addr_list,
+                       id1_col=args.id1_col,id2_col=args.id2_col,
+                       ibd_col=args.ibd_col,haploid_mode=args.haploid_mode,
+                       min_len=args.min_seg_len,sep=sep,removals=removals)
+    write_file(links,args.output,args.min_agg_len)
 
 
 if __name__ == '__main__':
     main()
+
+
